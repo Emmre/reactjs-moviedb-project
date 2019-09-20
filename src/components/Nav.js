31f2f666233c262d0c6cb02e6dbb5811f6cd7fc9
Emre <emreesrc@gmail.com>
2019-09-20 16:56:37 +0300

    reactjs-moviedb-project

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <nav>
+            <div class="navbar navbar-dark bg-dark">
+            <img src="https://p7.hiclipart.com/preview/59/893/319/film-clapperboard-cinematic-techniques-clapperboard-film-film-elements.jpg" alt="Smiley face" width="50" height="50"></img> 
+            <a href="/" class="Yazı" style={{fontSize: 40,  paddingRight: "940px", color:"white"}}>Movie Finder</a>  
+           
+            </div>
+        </nav>
+    )
+}
+
+
+export default Nav;
+
